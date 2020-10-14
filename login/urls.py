b92f13476a4d9ab1d1fb741fa28f6ff0631abfa1
Rafiat <danijurafiat@gmail.com>
2020-10-14 14:58:22 +0100

    make it better

--- login/urls.py
+++ login/urls.py
@@ -3,7 +3,7 @@ from .views import index, login, register, spider
 from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('', login, name="home"),
+    path('', index, name="home"),
     path('register/', register, name="register"),
     path('login/', LoginView.as_view(), name="login"),
     path('spider/', spider, name="spider_page"),
